351f5031bcd58914b8d9ffe08a8bd309f39c3354
MSRucoba <martinrucobaqupeu.edu.pe>
2022-10-03 17:56:29 -0500

    subiendomi proyecto springboot

---
+++ src/main/java/Martin/Rucoba/MRucobaApplication.java
@@ -0,0 +1,13 @@
+package Martin.Rucoba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MRucobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MRucobaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Martin/Rucoba/MRucobaApplicationTests.java
@@ -0,0 +1,13 @@
+package Martin.Rucoba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MRucobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
